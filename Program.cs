c5e6687c973fe47da9d82d7f68c2b8b4ea62e2ea
ander <ander@MYTHINKPAD>
2020-02-26 00:31:16 +0200

    Дбобавлен метод переворачивания 2- мерных массивов

--- Program.cs
+++ Program.cs
@@ -5,14 +5,44 @@ namespace DZ_20_02 {
 	class Program {
 		static void Main(string[] args) {
 			int[] array1 = new int[10] { 22, 3, 4, 55, 6, 7, 78, 9, 10, 7 };
+			int[,] sqrarr = {
+				{ 1, 1, 1, 2, 2, 2},
+				{ 1, 1, 1, 2, 2, 2},
+				{ 1, 1, 1, 2, 2, 2},
+				{ 3, 3, 3, 4, 4, 4},
+				{ 3, 3, 3, 4, 4,4 },
+				{ 3, 3, 3, 4, 4, 4}
+			};
 
-			//Console.WriteLine(AVG(array1));
-			//Console.WriteLine(SumEven(array1));
-
-			ReversArr(array1);
-			foreach (var item in array1) {
-				Console.Write(item + " ");
+			for (int i = 0; i <= sqrarr.GetUpperBound(0); i++) {
+				for (int j = 0; j <= sqrarr.GetUpperBound(1); j++) {
+					Console.Write(sqrarr[i, j] + "\t");
+				}
+				Console.WriteLine();
 			}
+
+			ReversArr(sqrarr);
+			Console.WriteLine("----------------------------------");
+
+			for (int i = 0; i <= sqrarr.GetUpperBound(0); i++) {
+				for (int j = 0; j <= sqrarr.GetUpperBound(1); j++) {
+					Console.Write(sqrarr[i, j] + "\t");
+				}
+				Console.WriteLine();
+			}
+
+
+
+
+			//ReversArr(sqrarr);
+
+			// Console.WriteLine(AVG(array1));
+			// Console.WriteLine(SumEven(array1));
+
+
+			//foreach (var item in ReversArr(array1)) {
+			//	Console.Write(item + " ");
+			//}
 
 
 
@@ -53,13 +83,26 @@ namespace DZ_20_02 {
 		}
 
 		// 4. для продвинутых: перевернуть массив.
+		static int[,] ReversArr(int[,] arr) {
+			int tmp;
+			for (int i = 0; i < arr.GetLength(0) / 2; i++) {
+				for (int j = 0; j < arr.GetLength(1) / 2; j++) {
+					tmp = arr[i, j];
+					arr[i, j] = arr[arr.GetLength(0) - i - 1, arr.GetLength(1) - j - 1];
+					arr[arr.GetLength(0) - i - 1, arr.GetLength(1) - j - 1] = tmp;
+				}
+
+			}
+
+			return arr;
+		}
+
 		static int[] ReversArr(int[] arr) {
-			int count = 0;
 			int tmp;
 			for (int i = 0; i < arr.Length / 2; i++) {
 				tmp = arr[i];
-				arr[i] = arr[arr.Length - i -1];
-				arr[arr.Length - i -1] = tmp;
+				arr[i] = arr[arr.Length - i - 1];
+				arr[arr.Length - i - 1] = tmp;
 			}
 
 			return arr;
